b280099d773f861d26306803d25e5414c01354a1
James Henderson <james@pcr.me.uk>
2013-04-30 21:25:51 +0100

    Decoding

--- src/vigenere/core.clj
+++ src/vigenere/core.clj
@@ -2,16 +2,13 @@
 
 (def a-to-z (map char (range (int \a )  (inc (int \z)))) )
 
-(def grid (take 26 (partition 26 1 (cycle a-to-z))) )
+(def grid (take 26 (partition 26 1 (cycle a-to-z))))
 
 (def grid-map 
   (zipmap a-to-z
-          (map (fn [letter-list]
-                 (zipmap a-to-z letter-list))
+          (map (fn [cypher-letters]
+                 (zipmap a-to-z cypher-letters))
                grid)))
-
-(comment
-  (take 100 (cycle a-to-z)))
 
 (defn encode-letter [letter key-letter]
   (get-in grid-map [key-letter letter]))
@@ -19,6 +16,25 @@
 (defn encode-text [plaintext passphrase]
   (when (seq passphrase)
     (apply str (map encode-letter plaintext (cycle passphrase)))))
+
+(encode-text "mytext" "key")
+
+(def reversed-grid-map 
+  (zipmap a-to-z
+          (map (fn [cypher-letters]
+                 (zipmap cypher-letters a-to-z))
+               grid)))
+
+(defn decode-letter [letter key-letter]
+  (get-in reversed-grid-map [key-letter letter]))
+
+(defn decode-text [plaintext passphrase]
+  (when (seq passphrase)
+    (apply str (map decode-letter plaintext (cycle passphrase)))))
+ 
+(decode-text "wcrobr" "key")
+
+(decode-text (encode-text "mytext" "key") "key")
 
 (comment
   (map + [1 2 3] [3 10 5 6 9]))
